d13dc4534902e28d1fe98ed2c9570d7174db6486
Svyatoslav Semenov <semenovslava293@gmail.com>
2024-05-11 20:55:43 +0300

    добавил визуализацию

--- task3/script.js
+++ task3/script.js
@@ -8,7 +8,17 @@ const submitButton = document.querySelector('#submitButton')
 
 const status = document.querySelector('#status')
 
+const fullCats = document.querySelector('#fullCats')
+const waitingCats = document.querySelector('#waitingCats')
+const foodImg = document.querySelector('#foodImg')
+const catFaceImg = document.querySelector('#catFaceImg')
+const fullBowlImg = document.querySelector('#fullBowlImg')
+const emptyBowlImg = document.querySelector('#emptyBowlImg')
+
 const wait = time => new Promise(r => setTimeout(r, time * 1000))
+
+const WAITING_CAT_IMG = '<img src="./images/waitingCat.svg" alt="Кошка">'
+const FULL_CAT_IMG = '<img src="./images/fullCat.svg" alt="Кошка">'
 
 const simulation = async () => {
     let currentCat = 1;
@@ -20,14 +30,22 @@ const simulation = async () => {
     const REFILL_FOOD_TIME = Number(r.value);
     const CAT_COUNT = Number(n.value)
 
+
+    const waitArray = []
+    for (let i = 0; i < CAT_COUNT - 1; i++) {
+        waitArray.push(WAITING_CAT_IMG)
+    }
+    waitingCats.innerHTML = waitArray.join('')
+    fullCats.innerHTML = ''
+
     const catGone = async (num, allTime) => {
-        status.innerHTML = `Котик ${num} ушел🐈`
+        status.innerHTML = `Котик ${num} уходит от миски🐈`
         const randomTime = Math.floor(Math.random() * 2) + 1
         await wait(randomTime)
         return allTime + randomTime
     }
     const catCame = async (num, allTime) => {
-        status.innerHTML = `Котик ${num} пришел😼`
+        status.innerHTML = `Котик ${num} подходит к миске😼`
         const randomTime = Math.floor(Math.random() * 2) + 1
         await wait(randomTime)
         return allTime + randomTime
@@ -37,30 +55,43 @@ const simulation = async () => {
     while (currentCat <= CAT_COUNT) {
         if (foodCount > 0) {
             if (currentCat !== 1) {
+                waitArray.pop();
+                waitingCats.innerHTML = waitArray.join('')
                 allTime = await catCame(currentCat, allTime)
             }
+            catFaceImg.classList.remove('hide')
             status.innerHTML = `Котик ${currentCat} кушает🥹`
             if (foodCount - (CAT_MUST_EAT - catAte) >= 0) {
                 await wait(EAT_TIME)
-                allTime+=EAT_TIME
+                allTime += EAT_TIME
+                catFaceImg.classList.add('hide')
                 if (currentCat + 1 <= CAT_COUNT) {
                     allTime = await catGone(currentCat, allTime)
                 }
-                currentCat += 1
+                fullCats.innerHTML += FULL_CAT_IMG
+                currentCat++
                 foodCount -= (CAT_MUST_EAT - catAte)
                 catAte = 0
             } else {
                 await wait(foodCount * EAT_TIME / CAT_MUST_EAT)
-                allTime+=foodCount * EAT_TIME / CAT_MUST_EAT
+                allTime += foodCount * EAT_TIME / CAT_MUST_EAT
                 catAte = foodCount
-                foodCount=0
+                foodCount = 0
+                catFaceImg.classList.add('hide')
                 allTime = await catGone(currentCat, allTime)
+                waitArray.push(WAITING_CAT_IMG)
+                waitingCats.innerHTML = waitArray.join('')
             }
-        }
-        else {
+        } else {
+            fullBowlImg.classList.add('hide')
+            emptyBowlImg.classList.remove('hide')
+            foodImg.classList.remove('hide')
             status.innerHTML = `Бабушка пополняет миску😐`
             await wait(REFILL_FOOD_TIME)
-            allTime+=REFILL_FOOD_TIME
+            emptyBowlImg.classList.add('hide')
+            foodImg.classList.add('hide')
+            fullBowlImg.classList.remove('hide')
+            allTime += REFILL_FOOD_TIME
             foodCount = MAX_FOOD_COUNT
         }
     }
@@ -75,8 +106,7 @@ form.onsubmit = async (event) => {
     if (Number(m.value) < Number(b.value)) {
         status.className = 'status__screen--error'
         status.innerHTML = `Вместительность меньше чем ест котик😡`
-    }
-    else if (!disabled) {
+    } else if (!disabled) {
         disabled = true;
         submitButton.disabled = true;
         await simulation();
